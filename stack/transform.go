358fe9624f5c3ed57b37e291440640e9d85794e2
Arran Schlosberg <aschlosberg@users.noreply.github.com>
2024-03-07 10:30:55 +0000

    Arbitrary stack transformations (#26)
    * feat: general-purpose stack transformations
    
    * doc: `ExampleTransformation`
    
    * doc: reference general-purpose stack transformations in README
    
    * chore: run `gazelle`
    
    * doc: demonstrate no-op transformations being elided

--- stack/transform.go
+++ stack/transform.go
@@ -13,22 +13,44 @@ type xFormType int
 const (
 	unknownXform xFormType = iota
 	permutation
+	general
 )
 
 // A Transformation transforms the stack by modifying its order, growing, and/or
 // shrinking it.
 type Transformation struct {
 	typ     xFormType
+	depth   uint8
 	indices []uint8
 	cache   string
 }
 
 // Permute returns a Transformation that permutes the order of the stack. The
 // indices MUST be a contiguous set of distinct values [0,n) in any order.
+//
+// While permutations can also be achieved with Transform(), Permute performs
+// additional checks on the indices (guaranteeing only SWAPs) and signals intent
+// to the reader of the code. See [Transformation] examples.
 func Permute(indices ...uint8) *Transformation {
 	return &Transformation{
 		typ:     permutation,
 		indices: indices,
+	}
+}
+
+// Transform returns a function that generates a general-purpose Transformation.
+// The `depth` specifies how deep in the stack the Transformation must modify.
+// The indices MUST all be less than `depth`.
+//
+// Note that the same indices with different depth will result in *different*
+// stack outputs. See [Transformation] examples.
+func Transform(depth uint8) func(indices ...uint8) *Transformation {
+	return func(i ...uint8) *Transformation {
+		return &Transformation{
+			typ:     general,
+			depth:   depth,
+			indices: i,
+		}
 	}
 }
 
@@ -42,6 +64,8 @@ func (t *Transformation) Bytecode() ([]byte, error) {
 	switch t.typ {
 	case permutation:
 		return t.permute()
+	case general:
+		return t.general()
 	default:
 		return nil, fmt.Errorf("invalid %T.typ = %d", t, t.typ)
 	}
@@ -57,6 +81,7 @@ func (t *Transformation) permute() ([]byte, error) {
 	if n := len(t.indices); n > 16 {
 		return nil, fmt.Errorf("can only permute up to 16 stack items; got %d", n)
 	}
+	t.depth = uint8(len(t.indices))
 
 	set := make(map[uint8]bool)
 	for _, idx := range t.indices {
@@ -74,12 +99,31 @@ func (t *Transformation) permute() ([]byte, error) {
 	return t.bfs(len(t.indices))
 }
 
+// general checks that t.depth and t.indices are valid for any transformation
+// and then returns t.bfs().
+func (t *Transformation) general() ([]byte, error) {
+	if t.depth > 16 {
+		return nil, fmt.Errorf("transformation depth %d > 16", t.depth)
+	}
+	for _, idx := range t.indices {
+		if idx >= t.depth {
+			return nil, fmt.Errorf("stack index %d beyond transformation depth of %d", idx, t.depth)
+		}
+	}
+
+	return t.bfs(int(t.depth))
+}
+
 // bfs performs a breadth-first search over a graph of stack-value orders,
 // starting from the root, in-order node [0, size). Edges represent nodes that
 // are reachable with only a single opcode.
 //
 // bfs should be called by the transformation-type-specific methods that first
 // check for valid indices. bfs itself is, however, type-agnostic.
+//
+// Although POP only uses 2 gas while DUPs/SWAPs use 3, there's no need for a
+// full Dijkstra implementation as changes in stack size can only be achieved by
+// POP/DUP and we limit graph edges accordingly.
 func (t *Transformation) bfs(size int) ([]byte, error) {
 	if size == 0 || size > 16 {
 		return nil, fmt.Errorf("invalid %T size %d", t, size)
@@ -105,9 +149,41 @@ func (t *Transformation) bfs(size int) ([]byte, error) {
 			return nil, fmt.Errorf("BUG: node %q in queue but not in graph", curr)
 		}
 
-		// SWAPs are limited to n-1 because they're 1-indexed in the stack
-		for i, n := 0, len(t.indices)-1; i < n; i++ {
-			op := vm.SWAP1 + vm.OpCode(i)
+		var edges []vm.OpCode
+		delta := want.deltas(curr)
+		currIndices := curr.toIndices()
+		allIndices := append(want.toIndices(), currIndices...)
+
+		for _, idx := range allIndices { // not ranging over delta, to avoid non-determinism
+			switch d := delta[idx]; {
+			case d == 0:
+				// counts match, may need a swap but no DUP/POP
+
+			case d > 0:
+				for i, cIdx := range currIndices {
+					if cIdx == idx {
+						edges = append(edges, vm.DUP1+vm.OpCode(i))
+						// We don't decrement delta because we can only make one
+						// change per queue loop. Since it's reachable with the
+						// op we've just added, there's no point following other
+						// edges.
+						delta[idx] = 0
+						break
+					}
+				}
+
+			case d < 0 && currIndices[0] == idx:
+				edges = append(edges, vm.POP)
+				delta[idx] = 0 // see rationale above
+			}
+		}
+
+		// SWAPs are limited to len-1 because they're 1-indexed in the stack
+		for i, n := 0, len(curr)-1; i < n; i++ {
+			edges = append(edges, vm.SWAP1+vm.OpCode(i))
+		}
+
+		for _, op := range edges {
 			next, err := curr.apply(op)
 			if err != nil {
 				return nil, err
@@ -168,6 +244,23 @@ func nodeFromIndices(is []uint8) node {
 	return node(s.String())
 }
 
+// toIndices is the inverse of nodeToIndices().
+func (n node) toIndices() []uint8 {
+	is := make([]uint8, len(n))
+	for i, rn := range n {
+		switch {
+		case '0' <= rn && rn <= '9':
+			is[i] = uint8(rn - '0')
+		case 'a' <= rn && rn <= 'f':
+			is[i] = uint8(rn - 'a' + 10)
+		default:
+			// See equivalent panic in nodeFromIndices().
+			panic(fmt.Sprintf("BUG: invalid %T rune %v; must be hex char", n, rn))
+		}
+	}
+	return is
+}
+
 // rootNode returns the node representing [0, …, size).
 func rootNode(size uint8) node {
 	buf := make([]byte, size)
@@ -177,15 +270,44 @@ func rootNode(size uint8) node {
 	return nodeFromIndices(buf)
 }
 
+// deltas returns a map keyed by every rune in the union of `n` and `o`, with
+// values describing the difference in counts in each. Those more prevalent in
+// `n` will have positive values, more-so in `o` will have negative values, and
+// runes that occur equally in both nodes will have zero as their value.
+//
+// The returned value indicates the number of DUPs / POPs required to convert
+// `o` to `n`.
+func (n node) deltas(o node) map[uint8]int {
+	d := make(map[uint8]int)
+	for _, i := range n.toIndices() {
+		d[i]++
+	}
+	for _, i := range o.toIndices() {
+		d[i]--
+	}
+	return d
+}
+
 // apply returns a *new* node equivalent to applying the opcode to n.
 func (n node) apply(o vm.OpCode) (node, error) {
 	switch base := o & 0xf0; {
+	case o == vm.POP:
+		return n[1:], nil
+
 	case base == vm.SWAP1:
 		out := make([]byte, len(n))
 		copy(out, []byte(n))
 
 		i := o - vm.SWAP1 + 1
 		out[0], out[i] = out[i], out[0] // invariants in the BFS loop guarantee that these are in range
+
+		return node(out), nil
+
+	case base == vm.DUP1:
+		out := make([]byte, len(n)+1)
+		copy(out[1:], []byte(n))
+
+		out[0] = out[o-vm.DUP1+1] // see above re invariants
 
 		return node(out), nil
 
--- stack/transform_test.go
+++ stack/transform_test.go
@@ -4,78 +4,230 @@ package stack_test
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/solidifylabs/specops"
 	"github.com/solidifylabs/specops/evmdebug"
 	"github.com/solidifylabs/specops/stack"
 )
 
+func ExampleTransformation() {
+	egs := []struct {
+		desc  string
+		xform *stack.Transformation
+	}{
+		{
+			desc:  "Permute",
+			xform: stack.Permute(2, 0, 3, 1),
+		},
+		{
+			desc: "Permute via Transform",
+			// Although this is equivalent to Permute(), its verbose, intent
+			// isn't clear, and there are no checks that it's a valid
+			// permutation.
+			xform: stack.Transform(4)(2, 0, 3, 1),
+		},
+		{
+			desc: "Transform same depth",
+			// Guaranteed *not* to have POPs because all stack items in [0,5)
+			// are used.
+			xform: stack.Transform(5)(4, 0, 2, 2, 3, 1),
+		},
+		{
+			desc: "Transform greater depth",
+			// Guaranteed to have POPs because, although the same indices as
+			// above, a greater stack depth is being transformed. Stack items
+			// {5,6} need to be removed.
+			xform: stack.Transform(7)(4, 0, 2, 2, 3, 1),
+		},
+		{
+			desc:  "Noop Transform",
+			xform: stack.Transform(2)(0, 1),
+		},
+		{
+			desc:  "Noop Permute",
+			xform: stack.Permute(0, 1, 2, 3, 4, 5),
+		},
+	}
+
+	for _, eg := range egs {
+		bytecode, err := eg.xform.Bytecode()
+		if err != nil {
+			log.Fatalf("%s error %v", eg.desc, err)
+		}
+
+		ops := make([]vm.OpCode, len(bytecode))
+		for i, b := range bytecode {
+			ops[i] = vm.OpCode(b)
+		}
+
+		fmt.Println(eg.desc, ops)
+	}
+
+	// Output:
+	// Permute [SWAP1 SWAP3 SWAP2]
+	// Permute via Transform [SWAP1 SWAP3 SWAP2]
+	// Transform same depth [DUP3 SWAP2 SWAP5]
+	// Transform greater depth [SWAP2 SWAP3 SWAP5 POP SWAP5 POP DUP2 SWAP3]
+	// Noop Transform []
+	// Noop Permute []
+}
+
 func intPtr(x int) *int {
 	return &x
 }
 
-func TestPermute(t *testing.T) {
+func TestTransformations(t *testing.T) {
 	type test struct {
+		name string
+		fn   func(...uint8) *stack.Transformation
+		// Setup of existing stack [0 … depth)
+		// For Permute(), depth == len(indices)
+		// Otherwise Transform(depth)
+		depth        int
 		indices      []uint8
-		wantNumSwaps *int // don't know when fuzzing so only test if non-nil
+		wantNumSteps *int // don't know when fuzzing so only test if non-nil
 	}
 
 	tests := []test{
 		{
+			name:         "noop Permute",
+			fn:           stack.Permute,
+			depth:        4,
 			indices:      []uint8{0, 1, 2, 3},
-			wantNumSwaps: intPtr(0),
+			wantNumSteps: intPtr(0),
 		},
 		{
+			name:         "single-SWAP Permute",
+			fn:           stack.Permute,
+			depth:        8,
 			indices:      []uint8{7, 1, 2, 3, 4, 5, 6, 0},
-			wantNumSwaps: intPtr(1),
+			wantNumSteps: intPtr(1),
 		},
 		{
+			name:         "single-SWAP Permute",
+			fn:           stack.Permute,
+			depth:        7,
 			indices:      []uint8{4, 1, 2, 3, 0, 5, 6},
-			wantNumSwaps: intPtr(1),
+			wantNumSteps: intPtr(1),
 		},
 		{
+			name:    "Permute",
+			fn:      stack.Permute,
+			depth:   4,
 			indices: []uint8{2, 1, 0, 3},
 		},
 		{
+			name:    "Permute",
+			fn:      stack.Permute,
+			depth:   4,
 			indices: []uint8{3, 2, 1, 0},
 		},
 		{
+			name:    "Permute",
+			fn:      stack.Permute,
+			depth:   7,
 			indices: []uint8{5, 0, 6, 3, 4, 2, 1},
+		},
+		{
+			name:         "single-POP Transform",
+			fn:           stack.Transform(5),
+			depth:        5,
+			indices:      []uint8{1, 2, 3, 4},
+			wantNumSteps: intPtr(1),
+		},
+		{
+			name:         "POP-all Transform",
+			fn:           stack.Transform(7),
+			depth:        7,
+			indices:      []uint8{},
+			wantNumSteps: intPtr(7),
+		},
+		{
+			name:         "single-DUP Transform",
+			fn:           stack.Transform(5),
+			depth:        5,
+			indices:      []uint8{3, 0, 1, 2, 3, 4},
+			wantNumSteps: intPtr(1),
+		},
+		{
+			name:    "Transform",
+			fn:      stack.Transform(5),
+			depth:   5,
+			indices: []uint8{1, 4, 0, 0, 1, 2, 0, 3},
+		},
+		{
+			name:    "Transform with POP",
+			fn:      stack.Transform(4),
+			depth:   4,
+			indices: []uint8{1, 3, 0, 3},
+		},
+		{
+			name:    "Transform with extra POPs",
+			fn:      stack.Transform(6),
+			depth:   6,
+			indices: []uint8{1, 3, 0, 3},
 		},
 	}
 
 	rng := rand.New(rand.NewSource(42))
+
 	for i := 0; i < 20; i++ {
 		in := []uint8{0, 1, 2, 3, 4, 5, 6, 7}
 		rng.Shuffle(len(in), func(i, j int) {
 			in[i], in[j] = in[j], in[i]
 		})
-		tests = append(tests, test{indices: in})
+
+		tests = append(tests, test{
+			name:    "Fuzz Permute",
+			fn:      stack.Permute,
+			depth:   len(in),
+			indices: in,
+		})
+	}
+
+	for i := 0; i < 50; i++ {
+		const depth = 5
+		indices := make([]uint8, rng.Intn(10))
+		for i := range indices {
+			indices[i] = uint8(rng.Intn(depth))
+		}
+
+		tests = append(tests, test{
+			name:    "Fuzz Transform",
+			fn:      stack.Transform(depth),
+			depth:   depth,
+			indices: indices,
+		})
 	}
 
 	for _, tt := range tests {
-		t.Run(fmt.Sprintf("%v", tt.indices), func(t *testing.T) {
+		tt := tt // for use with t.Parallel()
+		t.Run(fmt.Sprintf("%s n=%d %v", tt.name, tt.depth, tt.indices), func(t *testing.T) {
+			t.Parallel()
+
 			var code specops.Code
-			for i := range tt.indices { // explicitly not _, i
-				code = append(code, specops.PUSH(len(tt.indices)-i-1)) // {0 … n-1} top to bottom
+			for i := tt.depth; i > 0; i-- {
+				code = append(code, specops.PUSH(i-1)) // {0 … depth-1} top to bottom
 			}
 
-			perm := stack.Permute(tt.indices...)
-			code = append(code, perm)
+			xform := tt.fn(tt.indices...)
+			code = append(code, xform)
 
-			swaps, err := perm.Bytecode()
+			steps, err := xform.Bytecode()
 			if err != nil {
-				t.Fatalf("Bad test setup; Permute(%v).Bytecode() error %v", tt.indices, err)
+				t.Fatalf("Bad test setup; Permute/Transform(%v).Bytecode() error %v", tt.indices, err)
 			}
-			for _, s := range swaps {
+			for _, s := range steps {
 				t.Log(vm.OpCode(s))
 			}
-			if got := len(swaps); tt.wantNumSwaps != nil && got != *tt.wantNumSwaps {
-				t.Errorf("Permute(%v) got %d swaps; want %d", tt.indices, got, *tt.wantNumSwaps)
+			if got := len(steps); tt.wantNumSteps != nil && got != *tt.wantNumSteps {
+				t.Errorf("Permute/Transform(%v) got %d swaps; want %d", tt.indices, got, *tt.wantNumSteps)
 			}
 
 			dbg, _, err := code.StartDebugging(nil)
@@ -84,16 +236,16 @@ func TestPermute(t *testing.T) {
 			}
 			defer dbg.FastForward()
 
-			for i := 0; i < len(tt.indices); i++ {
+			for i := 0; i < tt.depth; i++ {
 				dbg.Step()
 			}
-			inOrder := make([]uint8, len(tt.indices))
+			inOrder := make([]uint8, tt.depth)
 			for i := range inOrder {
 				inOrder[i] = uint8(i)
 			}
 			t.Run("after PUSHing indices in order", stackTest(dbg, inOrder))
 
-			for i := 0; i < len(swaps); i++ {
+			for i := 0; i < len(steps); i++ {
 				dbg.Step()
 			}
 			t.Run("after SWAPing based on Permute()", stackTest(dbg, tt.indices))
@@ -124,7 +276,7 @@ func stackTest(dbg *evmdebug.Debugger, want8 []uint8) func(*testing.T) {
 		for i, w := range want8 {
 			want[i] = uint64(w)
 		}
-		if diff := cmp.Diff(want, got); diff != "" {
+		if diff := cmp.Diff(want, got, cmpopts.EquateEmpty()); diff != "" {
 			t.Errorf("Stack [top to bottom] diff (-want +got):\n%s", diff)
 		}
 	}
